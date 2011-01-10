03e7fb605632018c0d88aae4e6f4d8108c24fc07
Chris Benard <chris@chrisbenard.net>
2011-01-09 23:46:32 -0600

    Added install instructions to the actual site's code.

--- application/classes/controller/welcome.php
+++ application/classes/controller/welcome.php
@@ -1,10 +1,17 @@
-<?php defined('SYSPATH') or die('No direct script access.');
+<?php
+defined('SYSPATH') or die('No direct script access.');
 
 class Controller_Welcome extends Controller_Sitebase {
 
-	public function action_index()
-	{
-		$this->template->content = 'hello, world!';
-	}
+    public function action_index()
+    {
+        $this->template->content = 'hello, world!';
+    }
+
+    public function action_githubinstall()
+    {
+        $this->template->content = View::factory('githubinstall');
+        $this->template->title = "Installation Instructions";
+    }
 
 } // End Welcome
